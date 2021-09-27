2d0856ee2b832b44f09618a3b6640a218be56a1f
Justin Hui <75865779+justinhui0@users.noreply.github.com>
2021-09-26 22:43:41 -0400

    Add files via upload

--- game.py
+++ game.py
@@ -1,24 +1,47 @@
-#check if any player won
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import KFold
+from sklearn.metrics import accuracy_score, confusion_matrix, plot_confusion_matrix
+from sklearn.preprocessing import normalize, StandardScaler
+from sklearn.neighbors import KNeighborsClassifier, KNeighborsRegressor
+from sklearn.neural_network import MLPClassifier, MLPRegressor
+from sklearn.svm import SVC
+from sklearn.pipeline import make_pipeline
+import main
+import math
+#check if computer(2) or player(1) won
 def get_state(arr):
+    #horizontals(3)
     if arr[0] == arr[1] == arr[2] and arr[0] != 0:
+        #3 have lined up, if one of them is a 1, player won, else, computer won
         return arr[0]
     elif arr[3] == arr[4] == arr[5] and arr[3] != 0:
         return arr[3]
     elif arr[6] == arr[7] == arr[8] and arr[6] != 0:
         return arr[6]
-
+    
+    #verticals(3)
     elif arr[0] == arr[3] == arr[6] and arr[0] != 0:
-        return arr[0]
+        return arr[3]
+
     elif arr[1] == arr[4] == arr[7] and arr[1] != 0:
         return arr[1]
+
     elif arr[2] == arr[5] == arr[8] and arr[2] != 0:
         return arr[2]
 
+    #diagonals(2)
     elif arr[0] == arr[4] == arr[8] and arr[0] != 0:
         return arr[0]
+
     elif arr[2] == arr[4] == arr[6] and arr[2] != 0:
         return arr[2]
 
+    #Tie
+    elif arr[0] !=  0 and arr[1] !=  0 and arr[2] !=  0 and arr[3] !=  0 and arr[4] !=  0 and arr[5] !=  0 and arr[6] !=  0 and arr[7] !=  0 and arr[8] != 0:
+        return 3
+        
+    #no winner
     else:
         return 0
 
@@ -30,58 +53,80 @@ def printboard(arr):
           '{:2}'.format(arr[6]), " | ", '{:2}'.format(arr[7]), " | ", '{:2}'.format(arr[8]), "\n", sep='')
 
 
+def ValidMove(board, move):
+    if 0 > int(move) or int(move) > 8:
+        return False
+    elif board[int(move)] != 0:
+        return False
+    else:
+        return True
+
 def loadBoard():
     # array that represents board
     board = [0, 0, 0, 0, 0, 0, 0, 0, 0]
 
     # determines who's turn it is
     turn = input("0 to go first, 1 to go second:\n")
+    if turn != "0" and turn != "1":
+        turn = print("Error, please enter 0 or 1\n")       
+        
     #determines which regresser to play against
-    regressor = input("Enter KNN/linear/MLP")
+    regressor = input("Enter KNN or linear or MLP\n")
+    if regressor != "KNN" and regressor != "linear" and regressor != "MLP":
+        regressor = input("error, please enter KNN, linear or MLP\n")
+    #load data
+    tictac_multi_X, tictac_multi_y = main.load_dataset('tictac_multi.txt')
 
     while not get_state(board):
         if turn == "0":
             move = input("input?\n")
             # checking if valid move
-            if 0 > int(move) or int(move) > 8:
-                print("invalid move\n")
-            elif board[int(move)] != 0:
-                print("invalid move\n")
-
+            while(ValidMove(board, int(move)) == False):
+                move = input("invalid move\n")
             #update board
             else:
-                if move == "0":
-                    board[0] = 1
-                if move == "1":
-                    board[1] = 1
-                if move == "2":
-                    board[2] = 1
-                if move == "3":
-                    board[3] = 1
-                if move == "4":
-                    board[4] = 1
-                if move == "5":
-                    board[5] = 1
-                if move == "6":
-                    board[6] = 1
-                if move == "7":
-                    board[7] = 1
-                if move == "8":
-                    board[8] = 1
+                board[int(move)] = 1
                 turn = "1"
 
         elif turn == "1":
             if(regressor == "KNN"):
+                #get model
+                KNN = main.KNN_reg_get_model(tictac_multi_X, tictac_multi_y)
+                #predict best move 
+                best_moves = KNN.predict([board])[0]
+                moves_sorted = [(x, best_moves[x]) for x in range(len(best_moves))]
 
+                moves_sorted.sort(key = lambda x: x[1], reverse=True) 
+                print(moves_sorted)
+                chosen_move = -1
+                for move in moves_sorted:
+                    if ValidMove(board, move[0]):
+                        chosen_move = move[0]
+                        break
+
+                #set board
+                board[chosen_move] = -1
+                print("computer placed on %d\n" % chosen_move)
+                
             elif(regressor == "linear"):
-            
+                Linear = main.SVM_reg_get_model(tictac_multi_X, tictac_multi_y)
+                best_move = int(round((Linear.predict([board])[0])))
+                board[chosen_move] = -1
+                print("computer placed on %d\n" % chosen_move)
             elif(regressor == "MLP"):
-            
+                MLP = main.KNN_reg_get_model(tictac_multi_X, tictac_multi_y)
+
+
             turn = "0"
 
         printboard(board)
 
-    print("game over!")
+    if get_state(board) == 1:
+        print("game over! Player won!")
+    elif get_state(board) == -1:
+        print("game over! Computer won!")
+    elif get_state(board) == 3:
+        print("game over! Tie!")
 
 
 def play():
--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import KFold
-from sklearn.metrics import accuracy_score, confusion_matrix, plot_confusion_matrix
+from sklearn.metrics import accuracy_score, confusion_matrix, plot_confusion_matrix, multilabel_confusion_matrix
 from sklearn.preprocessing import normalize, StandardScaler
 from sklearn.neighbors import KNeighborsClassifier, KNeighborsRegressor
 from sklearn.neural_network import MLPClassifier, MLPRegressor
@@ -18,7 +18,7 @@ def load_dataset(name):
     y = data[:,9:]
     return (X, y)
 
-def k_fold_validation_classifier(k, X, y, model):
+def k_fold_validation(k, X, y, model, is_lin_regressor=False, is_multi=False):
     kf = KFold(n_splits=k)
     kf.get_n_splits(X)
     KFold(n_splits=k, random_state=None, shuffle=False)
@@ -28,23 +28,39 @@ def k_fold_validation_classifier(k, X, y, model):
         X_train, X_test = X[train_index], X[test_index]
         y_train, y_test = y[train_index], y[test_index]
 
-        model.fit(X_train,y_train.ravel())
-        pred_values = model.predict(X_test)
-        
-        acc = accuracy_score(pred_values , y_test)
-        acc_score.append(acc)
+        if not is_lin_regressor:
+            if is_multi:
+                model.fit(X_train,y_train)
+            else:
+                model.fit(X_train,y_train.ravel())
+            pred_values = model.predict(X_test)
 
-        conf_matrix = confusion_matrix(y_test, pred_values)
+            #TODO make sure works for all tests
+        else:
+            theta = np.linalg.inv(X.T.dot(X)).dot(X.T).dot(y)
+            pred_values = X_test.dot(theta)
+        
+        pred_values = pred_values.round().clip(0,8)
+
+        if is_multi:
+            acc = accuracy_score(np.array([pred_values , y_test]), np.ones((9, 9)))
+        else:
+            acc = accuracy_score(pred_values , y_test)
+        acc_score.append(acc)
+        if is_multi:
+            conf_matrix = multilabel_confusion_matrix(y_test, pred_values)
+        else:
+            conf_matrix = confusion_matrix(y_test, pred_values)
         conf_matrices.append(conf_matrix)
     avg_acc_score = sum(acc_score)/k
     mean_conf_matrix = np.mean(conf_matrices, axis=0)
-    normed_matrix = normalize(mean_conf_matrix, axis=1, norm='l1')
+    if is_multi:
+        normed_matrix = np.array([normalize(x, axis=1, norm='l1') for x in mean_conf_matrix])
+    else:
+        normed_matrix = normalize(mean_conf_matrix, axis=1, norm='l1')
     return avg_acc_score, normed_matrix
 
 
-def k_fold_validation_regressor(k, X, y, model):
-    # TODO implement k_fold for regression (or investigate how previous func can be made to work w/ regressors)
-    return (0,0)
 
 # CLASSIFIERS
 def KNN_class_get_model(X, y):
@@ -64,18 +80,14 @@ def SVM_class_get_model(X,y):
 
 # REGRESSORS
 def KNN_reg_get_model(X, y):
-    KNN = KNeighborsRegressor(n_neighbors=11)
-    KNN.fit(X, y.ravel())
+    KNN = KNeighborsRegressor(n_neighbors=1)
+    KNN.fit(X, y)
     return KNN
 
 def MLP_reg_get_model(X, y, alpha, hidden_layers, max_iter):
     MLP = MLPRegressor(solver='lbfgs', alpha=alpha, hidden_layer_sizes=hidden_layers, random_state=1, max_iter=max_iter)
-    MLP.fit(X,y.ravel())
+    MLP.fit(X,y)
     return MLP
-
-def SVM_reg_get_model(X, y):
-    # TODO implement with normal functions
-    pass
 
 #CLASSIFICATION
 
@@ -83,7 +95,7 @@ def SVM_reg_get_model(X, y):
 #KNN
 def run_knn_class_final_accuracy():
     knn_classifier = KNN_class_get_model(tictac_final_X, tictac_final_y)
-    acc_avg, norm_conf_matrix = k_fold_validation_classifier(10, tictac_final_X, tictac_final_y, knn_classifier)
+    acc_avg, norm_conf_matrix = k_fold_validation(10, tictac_final_X, tictac_final_y, knn_classifier)
     print("Accuracy score for tictac_final, KNN:\n%f" % acc_avg )
     print("Confusion matrix for tictac_final, KNN:")
     print(norm_conf_matrix)
@@ -91,7 +103,7 @@ def run_knn_class_final_accuracy():
 #MLP
 def run_mlp_class_final_accuracy():
     mlp_classifier = MLP_class_get_model(tictac_final_X, tictac_final_y, 1e-5, (5,2), 1000)
-    acc_avg, norm_conf_matrix = k_fold_validation_classifier(10, tictac_final_X, tictac_final_y, mlp_classifier)
+    acc_avg, norm_conf_matrix = k_fold_validation(10, tictac_final_X, tictac_final_y, mlp_classifier)
     print("Accuracy score for tictac_final, MLP:\n%f" % acc_avg )
     print("Confusion matrix for tictac_final, MLP:")
     print(norm_conf_matrix)
@@ -99,7 +111,7 @@ def run_mlp_class_final_accuracy():
 #SVM
 def run_svm_class_final_accuracy():
     SVM_classifier = SVM_class_get_model(tictac_final_X, tictac_final_y)
-    acc_avg, norm_conf_matrix = k_fold_validation_classifier(10, tictac_final_X, tictac_final_y, SVM_classifier)
+    acc_avg, norm_conf_matrix = k_fold_validation(10, tictac_final_X, tictac_final_y, SVM_classifier)
     print("Accuracy score for tictac_final, SVM:\n%f" % acc_avg )
     print("Confusion matrix for tictac_final, SVM:")
     print(norm_conf_matrix)
@@ -109,7 +121,7 @@ def run_svm_class_final_accuracy():
 #KNN
 def run_knn_class_single_accuracy():
     knn_classifier = KNN_class_get_model(tictac_single_X, tictac_single_y)
-    acc_avg, norm_conf_matrix = k_fold_validation_classifier(10, tictac_single_X, tictac_single_y, knn_classifier)
+    acc_avg, norm_conf_matrix = k_fold_validation(10, tictac_single_X, tictac_single_y, knn_classifier)
     print("Accuracy score for tictac_single, KNN Classifier:\n%f" % acc_avg )
     print("Confusion matrix for tictac_single, KNN Classifier:")
     print(norm_conf_matrix)
@@ -117,7 +129,7 @@ def run_knn_class_single_accuracy():
 #MLP
 def run_mlp_class_single_accuracy():
     mlp_classifier = MLP_class_get_model(tictac_single_X, tictac_single_y, 1e-5, (27,27,27), 4000)
-    acc_avg, norm_conf_matrix = k_fold_validation_classifier(10, tictac_single_X, tictac_single_y, mlp_classifier)
+    acc_avg, norm_conf_matrix = k_fold_validation(10, tictac_single_X, tictac_single_y, mlp_classifier)
     print("Accuracy score for tictac_single, MLP Classifier:\n%f" % acc_avg )
     print("Confusion matrix for tictac_single, MLP Classifier:")
     print(norm_conf_matrix)
@@ -125,7 +137,7 @@ def run_mlp_class_single_accuracy():
 #SVM
 def run_svm_class_single_accuracy():
     SVM_classifier = SVM_class_get_model(tictac_single_X, tictac_single_y)
-    acc_avg, norm_conf_matrix = k_fold_validation_classifier(10, tictac_single_X, tictac_single_y, SVM_classifier)
+    acc_avg, norm_conf_matrix = k_fold_validation(10, tictac_single_X, tictac_single_y, SVM_classifier)
     print("Accuracy score for tictac_single, SVM Classifier:\n%f" % acc_avg )
     print("Confusion matrix for tictac_single, SVM Classifier:")
     print(norm_conf_matrix)
@@ -138,23 +150,22 @@ def run_svm_class_single_accuracy():
 #KNN
 def run_knn_reg_single_accuracy():
     knn_classifier = KNN_reg_get_model(tictac_single_X, tictac_single_y)
-    acc_avg, norm_conf_matrix = k_fold_validation_regressor(10, tictac_single_X, tictac_single_y, knn_classifier)
+    acc_avg, norm_conf_matrix = k_fold_validation(10, tictac_single_X, tictac_single_y.round(), knn_classifier)
     print("Accuracy score for tictac_single, KNN Regressor:\n%f" % acc_avg )
     print("Confusion matrix for tictac_single, KNN Regressor:")
     print(norm_conf_matrix)
 
 #MLP
 def run_mlp_reg_single_accuracy():
-    mlp_classifier = MLP_reg_get_model(tictac_single_X, tictac_single_y, 1e-5, (27,27,27), 4000)
-    acc_avg, norm_conf_matrix = k_fold_validation_regressor(10, tictac_single_X, tictac_single_y, mlp_classifier)
+    mlp_classifier = MLP_reg_get_model(tictac_single_X, tictac_single_y, 1e-5, (27,27,27), 5000)
+    acc_avg, norm_conf_matrix = k_fold_validation(10, tictac_single_X, tictac_single_y, mlp_classifier)
     print("Accuracy score for tictac_single, MLP Regressor:\n%f" % acc_avg )
     print("Confusion matrix for tictac_single, MLP Regressor:")
     print(norm_conf_matrix)
 
 #SVM
 def run_svm_reg_single_accuracy():
-    svm_classifier = SVM_reg_get_model(tictac_single_X, tictac_single_y)
-    acc_avg, norm_conf_matrix = k_fold_validation_regressor(10, tictac_single_X, tictac_single_y, svm_classifier)
+    acc_avg, norm_conf_matrix = k_fold_validation(10, tictac_single_X, tictac_single_y, None, is_lin_regressor=True)
     print("Accuracy score for tictac_single, SVM Regressor:\n%f" % acc_avg )
     print("Confusion matrix for tictac_single, SVM Regressor:")
     print(norm_conf_matrix)
@@ -164,31 +175,29 @@ def run_svm_reg_single_accuracy():
 #KNN
 def run_knn_reg_multi_accuracy():
     knn_classifier = KNN_reg_get_model(tictac_multi_X, tictac_multi_y)
-    acc_avg, norm_conf_matrix = k_fold_validation_regressor(10, tictac_multi_X, tictac_multi_y, knn_classifier)
+    acc_avg, norm_conf_matrix = k_fold_validation(10, tictac_multi_X, tictac_multi_y, knn_classifier, is_multi=True)
     print("Accuracy score for tictac_multi, KNN Regressor:\n%f" % acc_avg )
     print("Confusion matrix for tictac_multi, KNN Regressor:")
     print(norm_conf_matrix)
 
 #MLP
 def run_mlp_reg_multi_accuracy():
-    mlp_classifier = MLP_reg_get_model(tictac_multi_X, tictac_multi_y, 1e-5, (27,27,27), 4000)
-    acc_avg, norm_conf_matrix = k_fold_validation_regressor(10, tictac_multi_X, tictac_multi_y, mlp_classifier)
+    mlp_classifier = MLP_reg_get_model(tictac_multi_X, tictac_multi_y, 1e-5, (27,27,27), 5000)
+    acc_avg, norm_conf_matrix = k_fold_validation(10, tictac_multi_X, tictac_multi_y, mlp_classifier,is_multi=True)
     print("Accuracy score for tictac_multi, MLP Regressor:\n%f" % acc_avg )
     print("Confusion matrix for tictac_multi, MLP Regressor:")
     print(norm_conf_matrix)
 
 #SVM
+#Ref: https://towardsdatascience.com/performing-linear-regression-using-the-normal-equation-6372ed3c57
 def run_svm_reg_multi_accuracy():
-    svm_classifier = SVM_reg_get_model(tictac_multi_X, tictac_multi_y)
-    acc_avg, norm_conf_matrix = k_fold_validation_regressor(10, tictac_multi_X, tictac_multi_y, svm_classifier)
+    acc_avg, norm_conf_matrix = k_fold_validation(10, tictac_multi_X, tictac_multi_y, None, is_multi=True,is_lin_regressor=True)
     print("Accuracy score for tictac_multi, SVM Regressor:\n%f" % acc_avg )
     print("Confusion matrix for tictac_multi, SVM Regressor:")
     print(norm_conf_matrix)
 
 # TODO 
 # produce confusion matrix plots
-# implement regressors on single and multi
-# tie regressors to tic tac game
 # create report/video
 # cite this https://users.auth.gr/~kehagiat/Research/GameTheory/12CombBiblio/TicTacToe.pdf
 # submit!
@@ -199,7 +208,13 @@ if __name__ == '__main__':
     tictac_final_X, tictac_final_y = load_dataset('tictac_final.txt')
     tictac_multi_X, tictac_multi_y = load_dataset('tictac_multi.txt')
     
-    run_mlp_reg_single_accuracy()
+    #run_knn_reg_single_accuracy()
+    #run_mlp_reg_single_accuracy()
+    #run_svm_reg_single_accuracy()
+
+    run_knn_reg_multi_accuracy()
+    run_mlp_reg_multi_accuracy()
+    run_svm_reg_multi_accuracy()
 
     #code skeleton for plotting, not sure how to implement w/ k-fold tho
     #plot_confusion_matrix(knn_classifier, testing_X, y_actual)
