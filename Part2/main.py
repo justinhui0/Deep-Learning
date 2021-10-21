2708b0896f840a4c4d11d955bb81326ed8fa4f4c
Cameron Burrows <cameron.burrows@planetpayment.com>
2021-10-20 20:29:50 -0400

    current implementation of nn

--- Part2/main.py
+++ Part2/main.py
@@ -4,29 +4,90 @@ import glob
 import numpy as np
 import random
 import torch.nn as nn
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
 import models
+
 
 #reduces memory requirements (32-bit float)
 torch.set_default_tensor_type(torch.FloatTensor)
 
-def load_dataset():
-    # retreive all face image files
-    img_dir = "face_images/*.jpg"
-    files = glob.glob(img_dir)
-    data = []
-    #eandomize order of images each time dataset is loaded
-    rand_indeces = torch.randperm(len(files))
-    data = [cv2.imread(files[i]) for i in rand_indeces]
+# defining the Dataset class
+class FaceImages(Dataset):
+    def __init__(self,train=True, augmentations=None):
+        self.train = train
 
-    #format data into a tensor of shape (750, 128, 128, 3)
-    data_stack = np.stack(data, axis=0)
-    faces_tensor = torch.tensor(data_stack)
+        # retreive all face image files
+        img_dir = "face_images/*.jpg"
+        files = glob.glob(img_dir)
+        data = []
+        #eandomize order of images each time dataset is loaded
+        rand_indeces = torch.randperm(len(files))
+        data = [cv2.imread(files[i]) for i in rand_indeces]
 
-    #split tensor into training and testing. 90% is training (675 training images, 75 testing images)
-    train_tensor = faces_tensor[:round(0.9 * len(faces_tensor))]
-    test_tensor = faces_tensor[round(0.9 * len(faces_tensor)):]
+        #format data into a tensor of shape (750, 128, 128, 3)
+        data_stack = np.stack(data, axis=0)
+        faces_tensor = torch.tensor(data_stack)
 
-    return (train_tensor,test_tensor)
+        #split tensor into training and testing. 90% is training (675 training images, 75 testing images)
+        if self.train:
+            self.data = faces_tensor[:round(0.9 * len(faces_tensor))]
+            if augmentations != None:
+                self.data = self.augment(augmentations)
+            self.data = self.convert_images(self.data)
+        else:
+            self.data = faces_tensor[round(0.9 * len(faces_tensor)):]
+            self.data = self.convert_images(self.data)
+  
+    def __len__(self):
+        return len(self.data)
+  
+    def __getitem__(self, index):
+        #Prepare converted dataset for training
+        img = self.data[index].numpy()
+        img = np.transpose(img,(2,0,1))
+        
+        input_vals = img[0:1,:,:] / 255
+
+
+        a_vals = img[1,:,:] / 255
+        b_vals = img[2,:,:] / 255
+
+        a_avg = a_vals.mean()
+        b_avg = b_vals.mean()
+
+        #construct and return input and output tensors for training and testing
+        output_arr = np.array([a_avg,b_avg])
+
+        input_tens = torch.tensor(input_vals).float()
+        output_tens = torch.tensor(output_arr).float()
+
+        return input_tens, output_tens
+
+    def augment(self, augmentations):
+        #iterate through provided tensor and apply the above augmentations yielding a tensor 10x as large as the input
+        augmented_arr = np.zeros((7500,128,128,3), int)
+        faces_arr = self.data.numpy()
+        for i,img in enumerate(faces_arr):
+            for j, augment in enumerate(augmentations):
+                augmented_arr[i*10 + j] = augment(img)
+        
+        augmented_arr = augmented_arr.astype(np.uint8)
+        return torch.tensor(augmented_arr)
+
+    #Image Conversion to L* A* B* Color Space
+    def convert_images(self, faces_tensor):
+        converted_tensor = torch.zeros_like(faces_tensor)
+        for i,img in enumerate(faces_tensor):
+            img = img.numpy()
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
+            converted_tensor[i] = torch.tensor(img)
+        
+        return converted_tensor
+
+
+
+
 
 #Augmentations 
 def random_crop(img):
@@ -67,61 +128,6 @@ augmentations = [
     lambda x: scale_rgb(random_crop(flip_vert(flip_horiz(x))))
 ]
 
-#Augment dataset
-def augment(faces_tensor):
-    #iterate through provided tensor and apply the above augmentations yielding a tensor 10x as large as the input
-    augmented_arr = np.zeros((7500,128,128,3), int)
-    faces_arr = faces_tensor.numpy()
-    for i,img in enumerate(faces_arr):
-        for j, augment in enumerate(augmentations):
-            augmented_arr[i*10 + j] = augment(img)
-    
-    augmented_arr = augmented_arr.astype(np.uint8)
-    augmented_tensor = torch.tensor(augmented_arr)
-
-    return augmented_tensor
-
-
-#Image Conversion to Color Space
-def convert_images(faces_tensor):
-    converted_tensor = torch.zeros_like(faces_tensor)
-    for i,img in enumerate(faces_tensor):
-        img = img.numpy()
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
-        converted_tensor[i] = torch.tensor(img)
-        
-
-    return converted_tensor
-
-#Prepare converted dataset for training
-def prepare_data(converted_tensor):
-    np_tens = converted_tensor.numpy()
-    # transpose array to expected format (7500, 3, 128, 128)
-    trans_arr = np.transpose(np_tens,(0,3,1,2))
-    #select the luminence values and normalize between [0,1]
-    input_vals = trans_arr[:,0:1,:,:] / 100
-
-    # select the A* and B* values then calculate the mean for expected values
-    a_vals = trans_arr[:,1,:,:]
-    b_vals = trans_arr[:,2,:,:]
-
-    a_avg = a_vals.mean(axis=tuple(range(1, a_vals.ndim)))
-    b_avg = b_vals.mean(axis=tuple(range(1, b_vals.ndim)))
-
-    #construct and return input and output tensors for training and testing
-    output_arr = np.array(list(zip(a_avg,b_avg)))
-
-    input_tens = torch.tensor(input_vals).float()
-    output_tens = torch.tensor(output_arr).float()
-    return (input_tens,output_tens)
-
-#split datasets into minibatches of size count
-def make_batches(input_tens, output_tens, count):
-    input_batches = torch.split(input_tens, count)
-    output_batches = torch.split(output_tens, count)
-
-    return tuple(zip(input_batches,output_batches))
-
 #display an inputted image
 def show_image(img):
     if not isinstance(img, np.ndarray):
@@ -160,35 +166,26 @@ def chrominance_regressor_main(model_path=""):
     if model_path == "":
         model_path = get_model_path("regmodel")
 
-    train_data, test_data = load_dataset()
-    print(" --- Data Loaded ---")
-    
-    if model_path is None:
-        augmented_tensor = augment(train_data)
-        print(" --- Data Augmented ---")
-        
-        converted_tensor = convert_images(augmented_tensor)
-        print(" --- Data Converted to LAB ---")
+    dataset = FaceImages(train=True,augmentations=augmentations)
 
-        prepared_tuple = prepare_data(converted_tensor)
-        training_batches = make_batches(*prepared_tuple, models.CHROMREG_MINIBATCH_SIZE)
-        print(" --- Data Prepared for Training ---")
-        
-        model = models.train_chrominance_reg(training_batches)
-        print(" --- Finished Training ---")
-    
-    else:
-        #load selected model
-        model = models.Chrominance_Regressor()
-        model.load_state_dict(torch.load(model_path))
-        model.eval()
 
-    #convert and prepare test data
-    lab_data = convert_images(test_data)
-    input_tens, output_tens = prepare_data(lab_data)
+    # implementing dataloader on the dataset and printing per batch
+    dataloader = DataLoader(dataset, batch_size=models.CHROMREG_MINIBATCH_SIZE, shuffle=True)
+
+
+    model = models.train_chrominance_reg(dataloader)
+    print(" --- Finished Training ---")
+    
+    # else:
+    #     #load selected model
+    #     model = models.Chrominance_Regressor()
+    #     model.load_state_dict(torch.load(model_path))
+    #     model.eval()
+
+    test_dataset = FaceImages(train=False)
 
     #compare expected and actual results of CNN performance on test data
-    results = model(input_tens)
+    results = model(test_dataset)
     for i,val in enumerate(zip(output_tens,results)):
         print("Test Image #{}:\tExpected:{}\tActual:{}".format(i+1, val[0].detach().numpy().round(4), val[1].detach().numpy().round(4)))
     criterion = nn.MSELoss()
@@ -236,6 +233,6 @@ if __name__ == '__main__':
     regressor = 1
     
     if regressor == 1:
-        chrominance_regressor_main(None)
+        chrominance_regressor_main()
     else:
         colorization_main()
