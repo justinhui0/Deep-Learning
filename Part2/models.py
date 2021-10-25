92eb5c0b46d710c11b66690d65055a8757d68670
Cameron Burrows <cameron.burrows@planetpayment.com>
2021-10-24 22:46:15 -0400

    adjusted

--- Part2/models.py
+++ Part2/models.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import numpy as np
 import cv2
 
-CHROMREG_MINIBATCH_SIZE = 8
+CHROMREG_MINIBATCH_SIZE = 10
 COLORIZE_MINIBATCH_SIZE = 8
 
 # REF: https://pytorch.org/tutorials/beginner/blitz/cifar10_tutorial.html
@@ -64,7 +64,7 @@ class Chrominance_Regressor(nn.Module):
 #Regressor
 def train_chrominance_reg(trainloader, device):
     EPOCH_COUNT = 24
-    LEARNING_RATE = 0.0001
+    LEARNING_RATE = 0.00015
 
     net = Chrominance_Regressor()
 
